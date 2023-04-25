8a7317e397cb9342fccd801970f2da62377a504e
SMCominotti <smcominotti@gmail.com>
2023-04-24 22:02:22 -0300

    express

--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -1,12 +1,43 @@
 import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from './action-types';
+import axios from 'axios';
 
+//esto lo borro en back
+// export const addFav = (character) => {
+//     return { type: ADD_FAV, payload: character }
+// };
+
+//esto lo agrego en back
 export const addFav = (character) => {
-    return { type: ADD_FAV, payload: character }
-};
+    const endpoint = 'http://localhost:3001/rickandmorty/fav';
+    return (dispatch) => {
+       axios.post(endpoint, character) // character es lo que recibo por body
+       .then(({ data }) => {
+          return dispatch({
+             type: ADD_FAV,
+             payload: data,
+          });
+       });
+    };
+ };
 
+ //esto se borra en back
+// export const removeFav = (id) => {
+//     return { type: REMOVE_FAV, payload: id }
+// }
+
+//esto se agrega en back
 export const removeFav = (id) => {
-    return { type: REMOVE_FAV, payload: id }
-}
+    const endpoint = 'http://localhost:3001/rickandmorty/fav/' + id;
+    return (dispatch) => {
+       axios.delete(endpoint)
+       .then(({ data }) => {
+          return dispatch({
+             type: REMOVE_FAV,
+             payload: data,
+         });
+       });
+    };
+ };
 
 export const filterCards = (gender) => {
     return { type: FILTER, payload: gender }
--- Client/src/redux/reducer.js
+++ Client/src/redux/reducer.js
@@ -10,15 +10,15 @@ const reducer = (state = initialState, { type, payload }) => {
         case ADD_FAV:
             return {
                 ...state,
-                myFavorites: [...state.allCharactersFav, payload],
-                allCharactersFav: [...state.allCharactersFav, payload]
+                myFavorites: payload,
+                allCharactersFav: payload
             }
 
         case REMOVE_FAV: 
             return {
                 ...state,
-                myFavorites: state.myFavorites.filter(fav => fav.id !== payload),
-                allCharactersFav: state.allCharactersFav.filter(fav => fav.id !== payload)
+                myFavorites: payload,
+                allCharactersFav: payload
             }
 
         case FILTER:
